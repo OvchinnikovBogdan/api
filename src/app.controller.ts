e5f1ce5ebbe2c8d254b58ba3ac62fdc42480e2c7
sha-255 <fawsgre@mail.ru>
2024-05-06 20:30:29 +0600

    Cors policy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): { mes: string } {
+    return { mes: this.appService.getHello() };
   }
 }
